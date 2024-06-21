e8800a45a987e9a66d026fc07a09261629d3d320
Zeb Hicks <Nimphious@gmail.com>
2024-06-21 17:00:53 +1000

    Minion spawner initial setup.

--- crates/minions/src/lib.rs
+++ crates/minions/src/lib.rs
@@ -1,16 +1,13 @@
 use bevy::prelude::*;
+use rand::prelude::*;
 
 pub struct MinionPlugin;
 
 impl Plugin for MinionPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
-		app.add_systems(Update, update);
+		app.add_systems(Update, spawn_minions);
     }
 }
-
-fn update() {}
-fn setup() {}
 
 pub enum MinionType {
     Melee1,
@@ -21,9 +18,29 @@ pub enum MinionType {
 
 #[derive(Component)]
 pub struct MinionSpawner {
-    pub spawn_rate: f32,
     pub spawn_timer: f32,
+    pub spawn_delay: f32,
     pub spawn_radius: f32,
     pub spawn_limit: i32,
     pub minion_type: MinionType,
+}
+
+fn spawn_minions(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut query: Query<(&Transform, &mut MinionSpawner)>,
+) {
+    for (transform, mut spawner) in query.iter_mut() {
+        spawner.spawn_timer -= time.delta_seconds();
+        if spawner.spawn_timer <= 0.0 {
+            let mut rng = rand::thread_rng();
+            let x = rng.gen_range(-spawner.spawn_radius..spawner.spawn_radius);
+            let y = rng.gen_range(-spawner.spawn_radius..spawner.spawn_radius);
+            commands.spawn(SpriteBundle {
+                transform: Transform::from_translation(Vec3::new(x, y, 0.0) + transform.translation),
+                ..Default::default()
+            });
+            spawner.spawn_timer += spawner.spawn_delay;
+        }
+    }
 }
