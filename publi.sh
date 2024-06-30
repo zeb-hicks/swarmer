2fc74269e763de034be614a203df961c8b8574ed
zeb-hicks <nimphious@gmail.com>
2024-07-01 06:50:59 +1000

    Switched to msvc toolchain for Windows builds.

--- publi.sh
+++ publi.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cargo build --target=x86_64-pc-windows-gnu --release
+export CARGO_FEATURE_PURE=1
+
+cargo build --target=x86_64-pc-windows-msvc --release
 cargo build --target=x86_64-unknown-linux-gnu --release
 
 rm -r target/publish/windows
