504f58b66ebd8a34badb30f923ea900a68be9f69
zeb-hicks <nimphious@gmail.com>
2024-07-02 03:49:43 +1000

    Hide windows console.

--- crates/game/src/main.rs
+++ crates/game/src/main.rs
@@ -1,4 +1,4 @@
-// #![windows_subsystem = "windows"]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use bevy::prelude::*;
 use entities::EntityPlugin;
@@ -10,7 +10,10 @@ use pixelate::PixelatePlugin;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_plugins(PixelatePlugin)
+        .add_plugins(PixelatePlugin {
+            width: 512,
+            height: 288,
+        })
         .add_plugins(PlayerPlugin)
         .add_plugins(InputPlugin)
         .add_plugins(MinionPlugin)
